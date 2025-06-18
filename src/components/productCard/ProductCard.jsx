e746fd21f08a7f6bce722f474c2fac808e5b759c
Dmytro Shaposhnikov <dimacr137@gmail.com>
2025-06-18 05:58:14 +0300

    add productModal and text validation

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -6,8 +6,8 @@ const ProductCard = ({product, onDelete, onClick}) => {
 			<img className="product__img" src={product.image} alt=""/>
 			<div className="product__text">
 				<h2 className="product__text-name">{product.name}</h2>
-				<p className="product__text-description">{product.description}</p>
-				<p className="product__text-rate">{product.quality}</p>
+				<p className="product__text-description">{product.description.slice(0, 35) + "..."}</p>
+				<p className="product__text-rate">Quality: {product.quality}</p>
 			</div>
 			<div className="product__buttons">
 				<button className="product__buttons-edit">Edit</button>
--- src/components/productModal/ProductModal.jsx
+++ src/components/productModal/ProductModal.jsx
@@ -1,9 +1,15 @@
 import './productModal.scss'
 
-const ProductModal = () => {
+const ProductModal = ({product, onClose}) => {
 	return (
-		<div className="modal">
-		
+		<div className="modal" onClick={onClose}>
+			<div className="modal__content" onClick={(e) => e.stopPropagation()}>
+				<img className="modal__content-img" src={product.image} alt={product.name} />
+				<h2 className="modal__content-header">{product.name}</h2>
+				<p className="modal__content-description">{product.description}</p>
+				<p className="modal__content-quality">Quality: {product.quality}</p>
+				<button className="modal__content-close" onClick={onClose}>Close</button>
+			</div>
 		</div>
 	)
 }
